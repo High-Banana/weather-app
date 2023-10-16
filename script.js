7426c2fa6689f1651927fbd170238a4a8976886f
Potassium <brainiacgamer444@gmail.com>
2023-10-16 23:44:04 +0545

    Finish adding weather info on DOM

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const countryNameElement = document.getElementById("country-name");
 const cityNameElement = document.getElementById("city-name");
 const weatherConditionElement = document.getElementById("current-weather");
 const feelsLikeTempElement = document.getElementById("feels-like-temperature");
+const secondaryTemperatureElement = document.getElementById("secondary-temperature");
 const currentTemperature = document.getElementById("temperature");
 const windSpeedElement = document.getElementById("wind-speed");
+const uvIndexElement = document.getElementById("uv-index");
+const visibilityDistanceElement = document.getElementById("visibility-distance");
 let userLocation;
 
 async function getWeatherInfo() {
@@ -26,6 +29,9 @@ async function getWeatherInfo() {
   const tempInFahrenheit = data.current.temp_f;
   const windInKPH = data.current.wind_kph;
   const windInMPH = data.current.wind_mph;
+  const uvIndex = data.current.uv;
+  const visibilityDistanceKM = data.current.vis_km;
+  const visibilityDistanceMiles = data.current.vis_miles;
   console.log(
     countryName,
     cityName,
@@ -35,16 +41,29 @@ async function getWeatherInfo() {
     tempInCelsius,
     tempInFahrenheit,
     windInKPH,
-    windInMPH
+    windInMPH,
+    uvIndex
   );
-  countryNameElement.textContent = countryName;
+  const tempDegreeUnit = document.createElement("span");
+  tempDegreeUnit.classList.add("temperature-unit");
+  tempDegreeUnit.innerHTML = "<sup>&degC</sup>";
+
+  const secondaryTempDegreeUnit = document.createElement("span");
+  secondaryTempDegreeUnit.classList.add("secondary-temperature-unit");
+  secondaryTempDegreeUnit.innerHTML = "&degF";
+
+  countryNameElement.textContent = countryName + ",";
   cityNameElement.textContent = cityName;
   weatherConditionElement.textContent = weatherCondition;
   feelsLikeTempElement.textContent = `Feels Like: ${feelsLikeInCelsius}`;
+  feelsLikeTempElement.appendChild(tempDegreeUnit);
   currentTemperature.textContent = tempInCelsius;
-  windSpeedElement.textContent = `Wind: ${windInKPH}`;
-
-  inputValue.value = "";
+  currentTemperature.appendChild(tempDegreeUnit.cloneNode(true));
+  windSpeedElement.textContent = `Wind: ${windInKPH} KPH`;
+  secondaryTemperatureElement.textContent = tempInFahrenheit;
+  secondaryTemperatureElement.appendChild(secondaryTempDegreeUnit);
+  uvIndexElement.textContent = `UV Index: ${uvIndex}`;
+  visibilityDistanceElement.textContent = `Visibility Distance: ${visibilityDistanceKM} KM`;
 }
 
 getWeatherInfo();
